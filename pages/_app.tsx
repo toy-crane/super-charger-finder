f906ce69d07b7b1889360818e5f0beb593e8d411
toycrane <alwaysfun2183@gmail.com>
2022-11-10 21:24:49 +0900

    feature: theme 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,25 @@
 import { css, Global } from "@emotion/react"
+import { ThemeProvider } from "@mui/material/styles"
 import type { AppProps } from "next/app"
 import Layout from "./layout"
 import globalStyle from "./style/global-style"
 import normalize from "./style/nomalize"
+import theme from "./style/theme"
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Global
-				styles={css`
-					${normalize}
-					${globalStyle}
-				`}
-			/>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</>
-	)
+  return (
+    <>
+      <Global
+        styles={css`
+          ${normalize}
+          ${globalStyle}
+        `}
+      />
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  )
 }
---
+++ pages/style/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#E82127",
+      main: "#CC0000",
+      dark: "#D3122D",
+    },
+    common: {
+      white: "#FFFFFF",
+      black: "#181B21",
+    },
+  },
+})
+
+export default theme
