5549b6947e5444ad0cc9a87e72b0597fdf48d493
toycrane <alwaysfun2183@gmail.com>
2022-11-11 11:20:08 +0900

    feature: Auto Complete와 연결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react"
 
 import { superChargerInfo } from "./data"
-import { Box, Container, Stack, useTheme } from "@mui/material"
+import { Container, Stack } from "@mui/material"
 import ChargingStationCard from "./components/card"
+import SearchInput from "./components/search-input"
 
 export default function Home() {
+  const [selectedStation, setSelectedStation] = useState<number>()
+
+  const handleSelectedStation = (id: number) => {
+    setSelectedStation(id)
+  }
+
+  const searchedStations = selectedStation
+    ? superChargerInfo.filter((station) => station.id === selectedStation)
+    : superChargerInfo
+
   return (
     <Container maxWidth="sm">
+      <SearchInput onInputChange={handleSelectedStation} />
       <Stack spacing={2} my={3}>
-        {superChargerInfo.map((item) => (
+        {searchedStations.map((item) => (
           <ChargingStationCard {...item} key={item.name} />
         ))}
       </Stack>
