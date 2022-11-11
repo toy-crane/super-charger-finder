0d825432291324c78e121eaa103ec14c01b23d69
toycrane <alwaysfun2183@gmail.com>
2022-11-11 11:18:26 +0900

    feature: dataset에 id 추가

--- pages/data.ts
+++ pages/data.ts
@@ -1,4 +1,5 @@
 export interface SuperCharger {
+  id: number
   region: "서울" | "경기" | "강원" | "충청" | "경북" | "경남" | "전라" | "제주"
   name: string
   location: string
@@ -9,6 +10,7 @@ export interface SuperCharger {
 
 const superChargerInfo: SuperCharger[] = [
   {
+    id: 1,
     region: "서울",
     name: "여의도 IFC 몰",
     location: "지하 7층 #64 기둥",
@@ -16,6 +18,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "120W",
   },
   {
+    id: 2,
     region: "서울",
     name: "강남 그랜드 인터컨티넨탈 호텔",
     location: "지하 4층",
@@ -23,6 +26,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "120W",
   },
   {
+    id: 3,
     region: "서울",
     name: "강남 압구정 안다르 호텔",
     location: "지하 4층",
@@ -30,6 +34,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "120W",
   },
   {
+    id: 4,
     region: "서울",
     name: "잠실 롯데 월드 타워",
     location: "지하 3층",
@@ -37,6 +42,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "120W",
   },
   {
+    id: 5,
     region: "서울",
     name: "잠실 롯데 백화점",
     location: "지하 3층 D3999",
@@ -44,6 +50,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "250W",
   },
   {
+    id: 6,
     region: "서울",
     name: "송파 소피텔 엠배세더",
     location: "지하 4층",
@@ -51,6 +58,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "250W",
   },
   {
+    id: 7,
     region: "서울",
     name: "용산 그랜드 하얏트 호텔",
     location: "지하 2층",
@@ -58,6 +66,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "250W",
   },
   {
+    id: 8,
     region: "서울",
     name: "종각 그랑서울",
     location: "지하 6층",
@@ -66,6 +75,7 @@ const superChargerInfo: SuperCharger[] = [
     DcCount: 3,
   },
   {
+    id: 9,
     region: "서울",
     name: "김포공항 롯데몰",
     location: "지하 1층 B15",
@@ -73,6 +83,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "120W",
   },
   {
+    id: 10,
     region: "서울",
     name: "테헤란 강남 N타워",
     location: "지하 4층",
@@ -80,6 +91,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "120W",
   },
   {
+    id: 11,
     region: "서울",
     name: "역삼 센터필드",
     location: "지하 5층",
@@ -88,6 +100,7 @@ const superChargerInfo: SuperCharger[] = [
     DcCount: 12,
   },
   {
+    id: 12,
     region: "서울",
     name: "왕십리 비트플렉스",
     location: "지하 5층",
@@ -96,6 +109,7 @@ const superChargerInfo: SuperCharger[] = [
     DcCount: 3,
   },
   {
+    id: 13,
     region: "서울",
     name: "구로 넷마블",
     location: "지하 4층",
@@ -104,6 +118,7 @@ const superChargerInfo: SuperCharger[] = [
     DcCount: 3,
   },
   {
+    id: 14,
     region: "서울",
     name: "서초 쌍용 플레티넘",
     location: "지하 2층",
@@ -112,6 +127,7 @@ const superChargerInfo: SuperCharger[] = [
     DcCount: 8,
   },
   {
+    id: 15,
     region: "서울",
     name: "강북 파라스파라",
     location: "지상 1,2층",
@@ -119,6 +135,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "250W",
   },
   {
+    id: 16,
     region: "서울",
     name: "신사 스퀘어",
     location: "지하 4층",
@@ -126,6 +143,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "250W",
   },
   {
+    id: 17,
     region: "서울",
     name: "청량리 롯데백화점",
     location: "M5F",
@@ -134,6 +152,7 @@ const superChargerInfo: SuperCharger[] = [
     DcCount: 6,
   },
   {
+    id: 18,
     region: "서울",
     name: "여의도 CCMM",
     location: "지하 6층",
@@ -141,6 +160,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "250W",
   },
   {
+    id: 19,
     region: "서울",
     name: "수유 에피소드 수유838",
     location: "지하 4층",
@@ -149,6 +169,7 @@ const superChargerInfo: SuperCharger[] = [
     DcCount: 2,
   },
   {
+    id: 20,
     region: "서울",
     name: "신림 타임스트림",
     location: "지하 6층",
@@ -157,6 +178,7 @@ const superChargerInfo: SuperCharger[] = [
     DcCount: 2,
   },
   {
+    id: 21,
     region: "서울",
     name: "금천 마리오아울렛",
     location: "1관 지하 3층",
@@ -164,6 +186,7 @@ const superChargerInfo: SuperCharger[] = [
     chargingSpeed: "250W",
   },
   {
+    id: 22,
     region: "서울",
     name: "성수 D타워",
     location: "지하 5층",
