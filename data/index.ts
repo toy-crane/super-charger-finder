eeb7d36fc17577fa250988d4e9c092bf667abae0
toycrane <alwaysfun2183@gmail.com>
2022-11-18 17:06:52 +0900

    feature: rawData에 주차비 정보 추가

--- data/index.ts
+++ data/index.ts
@@ -3,6 +3,8 @@ export interface SuperCharger {
   region: "서울" | "경기" | "강원" | "충청" | "경북" | "경남" | "전라" | "제주"
   name: string
   freeParkingChargeTime: number
+  parkingFee?: string
+  parkingFeeDiscount?: string
   chargingSpeed: "120W" | "250W"
   DcCount?: number
   locationId: string
@@ -28,6 +30,9 @@ const superChargerInfo: SuperCharger[] = [
     freeParkingChargeTime: 30,
     chargingSpeed: "120W",
     stallCount: 5,
+    parkingFee:
+      "초과 10분당 1,000원 \n 2만원 이상 1시간 무료 \n 4만원 이상 2시간 무료",
+    parkingFeeDiscount: "1만원 구매 시 1시간 무료",
     address: {
       street: "서울특별시 영등포구 국제금융로 10",
       locationDetail: "지하 7층 #64 기둥",
@@ -47,6 +52,9 @@ const superChargerInfo: SuperCharger[] = [
     freeParkingChargeTime: 20,
     chargingSpeed: "120W",
     stallCount: 5,
+    parkingFee: "기본 30분 4,500원 \n 이후 10분당 1,500원",
+    parkingFeeDiscount:
+      "호텔 1층 그랜드 델리 2만원 구입 시, 2시간 무료 주차 \n 코엑스몰 식당 할인 안됨",
     address: {
       street: "서울특별시 강남구 테헤란로 521",
       city: "서울",
@@ -65,6 +73,7 @@ const superChargerInfo: SuperCharger[] = [
     name: "강남 압구정 안다르 호텔",
     freeParkingChargeTime: 15,
     chargingSpeed: "120W",
+    parkingFee: "기본 30분 4,000원 \n 이후 10분당 2,000원",
     address: {
       street: "서울특별시 강남구 논현로 854",
       city: "서울",
@@ -84,6 +93,8 @@ const superChargerInfo: SuperCharger[] = [
     locationId: "seouljamsilsupercharger",
     freeParkingChargeTime: 0,
     chargingSpeed: "120W",
+    parkingFee: "10시 ~ 20시: 10분당 500원 \n 그 외 시간: 10분당 200원",
+    parkingFeeDiscount: "문화 시설 이외에는 주차권 미지급",
     address: {
       street: "서울특별시 올림픽로 300",
       city: "서울",
@@ -103,6 +114,8 @@ const superChargerInfo: SuperCharger[] = [
     locationId: "seoulsongpaolympicparksupercharger",
     freeParkingChargeTime: 0,
     chargingSpeed: "250W",
+    parkingFee: "기본 30분 1,000원 \n 초과 10분당 1,000원",
+    parkingFeeDiscount: "1/3/5만원 마트 구매 시 1/2/3시간 무료",
     address: {
       street: "송파구 올림픽로 240",
       city: "서울",
@@ -120,6 +133,8 @@ const superChargerInfo: SuperCharger[] = [
     region: "서울",
     name: "송파 소피텔 엠배세더",
     locationId: "songpasupercharger",
+    parkingFee: "10분당 1,000원",
+    parkingFeeDiscount: "업장 이용 시 1-2시간 무료 (스타벅스 제외)",
     address: {
       street: "서울특별시 잠실로 209",
       locationDetail: "지하 4층",
@@ -141,6 +156,8 @@ const superChargerInfo: SuperCharger[] = [
     freeParkingChargeTime: 15,
     chargingSpeed: "250W",
     locationId: "seoulyongsansupercharger",
+    parkingFee: "30분 당 4,000원 이후 10분당 2,5000원",
+    parkingFeeDiscount: "투숙객 할인 안됨",
     address: {
       street: "용산구 소월로 322",
       locationDetail: "지하 2층",
@@ -161,6 +178,9 @@ const superChargerInfo: SuperCharger[] = [
     freeParkingChargeTime: 10,
     chargingSpeed: "120W",
     DcCount: 3,
+    parkingFee: "평일 10분당 1,000원 \n 주말 1시간 1,000원 12시간 5,000원",
+    parkingFeeDiscount:
+      "카페, 편의시설 이용 시 1시간 무료 \n 식당 이용 시 2시간 무료",
     address: {
       street: "서울특별시 종로구 종로 33",
       city: "서울",
@@ -181,6 +201,8 @@ const superChargerInfo: SuperCharger[] = [
     freeParkingChargeTime: 20,
     chargingSpeed: "250W",
     DcCount: 3,
+    parkingFee: "30분 3,000원 추가 10분당 1,000원",
+    parkingFeeDiscount: "편의점만 사용해도 2시간 무료 주차",
     address: {
       street: "왕십리로 83-21",
       city: "서울",
@@ -200,6 +222,8 @@ const superChargerInfo: SuperCharger[] = [
     locationId: "seoulteheransupercharger",
     freeParkingChargeTime: 0,
     chargingSpeed: "120W",
+    parkingFee: "30분 3,000원 추가 10분당 1,000원",
+    parkingFeeDiscount: "업장 이용 시 2시간 무료",
     address: {
       street: "서울특별시 강남구 테헤란로 129",
       city: "서울",
@@ -220,6 +244,8 @@ const superChargerInfo: SuperCharger[] = [
     freeParkingChargeTime: 30,
     chargingSpeed: "250W",
     DcCount: 12,
+    parkingFee: "10분당 1,500원",
+    parkingFeeDiscount: "업장 이용 시 2시간 무료 \n 스타벅스 1시간 무료",
     address: {
       street: "테헤란로 231",
       city: "서울",
@@ -240,6 +266,9 @@ const superChargerInfo: SuperCharger[] = [
     freeParkingChargeTime: 30,
     chargingSpeed: "250W",
     DcCount: 3,
+    parkingFee: "10분당 1,000원",
+    parkingFeeDiscount:
+      "3만원 미만 구매 시 1시간 무료 \n 3만원 이상 구매 시 2시간 무료",
     address: {
       street: "성동구 왕십리광장로 17",
       city: "서울",
@@ -260,6 +289,8 @@ const superChargerInfo: SuperCharger[] = [
     freeParkingChargeTime: 15,
     chargingSpeed: "250W",
     DcCount: 3,
+    parkingFee: "10분당 700원",
+    parkingFeeDiscount: "업장 이용 시 1시간 무료",
     address: {
       street: "디지털로26길 38",
       city: "서울",
@@ -283,6 +314,7 @@ const superChargerInfo: SuperCharger[] = [
       state: null,
       locationDetail: "지하 2층",
     },
+    parkingFee: "기본 5,000원 \n 50분 초과 시, 10분당 1,000원",
     gps: {
       latitude: 37.48406,
       longitude: 127.01732,
@@ -297,6 +329,8 @@ const superChargerInfo: SuperCharger[] = [
     region: "서울",
     name: "강북 파라스파라",
     locationId: "gangbuksupercharger",
+    parkingFee: "10분당 1,000원",
+    parkingFeeDiscount: "업장 이용 시 3시간 무료",
     address: {
       locationDetail: "지상 1,2층",
       street: "삼양 689",
@@ -318,6 +352,7 @@ const superChargerInfo: SuperCharger[] = [
     name: "신사 스퀘어",
     freeParkingChargeTime: 30,
     chargingSpeed: "250W",
+    parkingFee: "30분당 3,000원 추가 10분당 1,000원",
     address: {
       street: "강남대로 652 신사 스퀘어",
       city: "서울",
@@ -337,6 +372,9 @@ const superChargerInfo: SuperCharger[] = [
     locationId: "seoulcheongnyangrisupercharger",
     freeParkingChargeTime: 30,
     chargingSpeed: "250W",
+    parkingFee: "30분당 1,000원, 10분당 1,000원",
+    parkingFeeDiscount:
+      "1/5/10만원 구매 시, 1/2/3시간 무료 \n 영화 3시간 \n 식당 2시간",
     DcCount: 6,
     address: {
       street: "서울특별시 왕산로 214",
@@ -357,6 +395,7 @@ const superChargerInfo: SuperCharger[] = [
     locationId: "seoulccmmsupercharger",
     freeParkingChargeTime: 10,
     chargingSpeed: "250W",
+    parkingFee: "10분당 1,000원",
     address: {
       street: "Seoul 여의공원로 101",
       locationDetail: "지하 6층",
@@ -374,6 +413,7 @@ const superChargerInfo: SuperCharger[] = [
     region: "서울",
     name: "수유 에피소드 수유838",
     locationId: "suyusupercharger",
+    parkingFee: "15분당 1,000원",
     freeParkingChargeTime: 10,
     chargingSpeed: "250W",
     DcCount: 2,
@@ -396,6 +436,8 @@ const superChargerInfo: SuperCharger[] = [
     locationId: "seoultimesstreamsupercharger",
     freeParkingChargeTime: 20,
     chargingSpeed: "250W",
+    parkingFee: "최초 30분 1,000원, 10분당 1,000원",
+    parkingFeeDiscount: "1/3/5만원 구매 시, 1/2/3시간 무료",
     DcCount: 2,
     address: {
       street: "신림로 330",
@@ -414,6 +456,8 @@ const superChargerInfo: SuperCharger[] = [
     region: "서울",
     locationId: "geumcheonsupercharger",
     name: "금천 마리오아울렛",
+    parkingFee: "30분 이후 10분당 500원",
+    parkingFeeDiscount: "5/10/20/30/40/50만원 구매 시, 1/2/3/4/5/6시간 무료",
     address: {
       street: "디지털로9 23 마리오 아울렛",
       city: "서울",
