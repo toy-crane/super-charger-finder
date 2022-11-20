464cc3e0d504812456626dafc1fb1c9232174c32
toycrane <alwaysfun2183@gmail.com>
2022-11-21 00:37:51 +0900

    feature: favicon 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import ChargingStationCard from "../components/card"
 import SearchInput from "../components/search-input"
 import { superChargerInfo } from "../data"
 import StationModal from "../components/station-modal"
+import Head from "next/head"
 
 export default function Home() {
   const [selectedStationId, setSelectedStationId] = useState<number>()
@@ -40,6 +41,42 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>가장 효율적인 슈퍼차저를 찾아 보세요</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Super charger Finder" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <SearchInput onInputChange={handleSearchedStation} />
       <Stack spacing={2} my={3}>
         {searchedStations.map((item) => (
