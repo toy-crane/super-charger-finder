cc865f9f9fb6fe6251407af0a4ce82a5a14a93af
toycrane <alwaysfun2183@gmail.com>
2022-11-21 00:06:10 +0900

    feature: style 폴더 이동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import { css, Global } from "@emotion/react"
 import { ThemeProvider } from "@mui/material/styles"
 import type { AppProps } from "next/app"
 import Layout from "../components/layout"
-import globalStyle from "./style/global-style"
-import normalize from "./style/nomalize"
-import theme from "./style/theme"
+import globalStyle from "../style/global-style"
+import normalize from "../style/nomalize"
+import theme from "../style/theme"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
