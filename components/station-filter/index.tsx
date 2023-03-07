d78f690a8b42e8076ffa1a42e7ef90b8b29d0db1
toycrane <alwaysfun2183@gmail.com>
2023-03-08 00:45:40 +0900

    feat: stationFilterValuesState 위치 변경

--- components/station-filter/index.tsx
+++ components/station-filter/index.tsx
@@ -3,15 +3,56 @@ import Filter from "@mui/material/Chip"
 import Stack from "@mui/material/Stack"
 import { Box } from "@mui/material"
 
-import { useRecoilValue } from "recoil"
+import { selector, useRecoilValue } from "recoil"
 import {
   FilterFieldsValue,
-  stationFilterValuesState,
+  stationFilterState,
 } from "../../atoms/station-filter"
 import StateFilter from "./state-filter"
 import PowerFilter from "./power-filter"
 import HasDiscountFilter from "./has-discount-filter"
 import FreeParkingFilter from "./free-parking-filter"
+
+export const stationFilterValuesState = selector<FilterFieldsValue[]>({
+  key: "stationFilterValues",
+  get: ({ get }) => {
+    const { states, powers, hasDiscount, freeParkingTime } =
+      get(stationFilterState)
+    return [
+      {
+        value: "states",
+        label: states.length > 0 ? states.join(", ") : "지역",
+        active: states.length > 0,
+      },
+      {
+        value: "powers",
+        label:
+          powers.length > 0
+            ? `속도 ${powers.map((p) => `${p}W`).join(", ")}`
+            : "충전속도",
+        active: powers.length > 0,
+      },
+      {
+        value: "hasDiscount",
+        label:
+          hasDiscount === undefined
+            ? "주차 할인"
+            : hasDiscount
+            ? "주차 할인 있음"
+            : "주차 할인 없음",
+        active: hasDiscount !== undefined,
+      },
+      {
+        value: "freeParkingTime",
+        label:
+          freeParkingTime.length > 1
+            ? `회차 ${freeParkingTime[0]}분 ~ ${freeParkingTime[1]}분`
+            : "회차 시간",
+        active: freeParkingTime.length > 1,
+      },
+    ]
+  },
+})
 
 export default function StationFilter() {
   const stationFilterValues = useRecoilValue(stationFilterValuesState)
